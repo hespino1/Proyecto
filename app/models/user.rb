9c0fc08d01da9007a80038d80086aff6ce466914
Ruben Espinosa <rderoldan1@gmail.com>
2012-05-08 18:03:58 -0500

    validacion de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :nombre, :presence => true
+  validates :correo, :presence => true
+  validates :cedula, :presence => true, :numericality => true
+  validates :password, :presence => true,
+
+
 end
